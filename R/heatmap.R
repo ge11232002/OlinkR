28276486f1812b39744b632bcb48877265c2d9ce
Ge Tan <gtan@me.com>
2020-08-02 22:33:24 +0200

    Add overview heatmap

--- R/heatmap.R
+++ R/heatmap.R
@@ -6,7 +6,7 @@
 #' @param se A \code{\link[SummarizedExperiment]{SummarizedExperiment}} object.
 #' @param p.value \code{numeric}(1): The p.value cutoff for significance level.
 #' @param log2FC \code{numeric}(1): The log2FC cutoff for significance level.
-#' @param filename \code{character}(1): The filename of heatmap output.
+#' @param ... graphical parameter passed to \code{pheatmap}.
 #' @importFrom pheatmap pheatmap
 #' @importFrom scater uniquifyFeatureNames
 #' @export
@@ -23,7 +23,7 @@
 #'                   blocking="Donor_Factor")
 #' olink_heatmap(tb, se)
 
-olink_heatmap <- function(tb, se, p.value=0.05, log2FC=0, filename=NA){
+olink_heatmap <- function(tb, se, p.value=0.05, log2FC=0, ...){
   tb <- tb %>% filter(P.Value < p.value, isPresent, abs(logFC) >= log2FC)
   if(nrow(tb) == 0L){
     return(NULL)
@@ -37,8 +37,51 @@ olink_heatmap <- function(tb, se, p.value=0.05, log2FC=0, filename=NA){
   p <- pheatmap(toPlot, color=colorRampPalette(c("blue", "white", "red"))(100),
                 scale="row", annotation_col=annotation_col,
                 cluster_rows=if_else(nrow(toPlot) >=2, TRUE, FALSE),
-                cluster_cols=FALSE, clustering_distance_rows="correlation",
-                filename=filename,
-                cellwidth=10, cellheight=10)
+                cluster_cols=FALSE, ...)
+  return(p)
+}
+
+#' A overview of NPX value in heatmap
+#'
+#' Plot the NPX value in a heatmap to spot any potential clustering and outliers
+#' @param se A \code{\link[SummarizedExperiment]{SummarizedExperiment}} object.
+#' @param scale \code{character}(1): how to scale (z-score) the data. "none": no scaling.
+#'                                   "sample": scale across the samples.
+#'                                   "feature": scale across the features (proteins).
+#' @param cluster_samples \code{logical}(1): do the samples clustering.
+#' @param cluster_features \code{logical}(1): do the samples clustering.
+#' @param ... graphical parameter passed to \code{pheatmap}.
+#' @importFrom pheatmap pheatmap
+#' @importMethodsFrom SummarizedExperiment assay
+#' @importFrom scater uniquifyFeatureNames
+#' @export
+#' @return A \code{pheatmap} object.
+#' @author Ge Tan
+#' @examples
+#' npxFn <- system.file("extdata", c("20200507_Inflammation_NPX_1.xlsx",
+#'                                   "20200625_Inflammation_NPX_2.xlsx"),
+#'                      package="OlinkR")
+#' metaFn <- system.file("extdata", "Inflammation_Metadata.xlsx", package="OlinkR")
+#' se <- readNPX(npxFn, metaFn)$SummarizedExperiment
+#' overview_heatmap(se)
+overview_heatmap <- function(se, scale=c("none", "row", "column"),
+                             cluster_samples=FALSE, cluster_features=FALSE,
+                             ...){
+  scale <- match.arg(scale)
+
+  toPlot <- assay(se, "npx")
+  rownames(toPlot) <- uniquifyFeatureNames(rownames(se), rowData(se)$Assay)
+  annotation_col <- data.frame(colData(se), check.names = FALSE)
+
+  if(scale == "none"){
+    p <- pheatmap(toPlot, scale=scale, annotation_col=annotation_col,
+                  cluster_rows=cluster_features, cluster_cols=cluster_samples,
+                  ...)
+  }else{
+    p <- pheatmap(toPlot, color=colorRampPalette(c("blue", "white", "red"))(100),
+                  scale=scale, annotation_col=annotation_col,
+                  cluster_rows=cluster_features, cluster_cols=cluster_samples,
+                  ...)
+  }
   return(p)
 }
